db76882b05e5b09dd501a7c672e2fbdd6fd9e3c9
Chengliang Zhang <marcoszh@users.noreply.github.com>
2024-04-22 19:01:26 +0800

    Update libs/cohere/langchain_cohere/chat_models.py
    Co-authored-by: harry-cohere <127103098+harry-cohere@users.noreply.github.com>
    Signed-off-by: Chengliang Zhang <marcoszh@users.noreply.github.com>

--- libs/cohere/langchain_cohere/chat_models.py
+++ libs/cohere/langchain_cohere/chat_models.py
@@ -410,8 +410,8 @@ class ChatCohere(BaseChatModel, BaseCohere):
             )
         if hasattr(response, "token_count"):
             generation_info["token_count"] = response.token_count
-        elif hasattr(response.meta, "tokens"):
-            generation_info["token_count"] = response.meta.tokens
+        elif hasattr(response.meta, "tokens") and response.meta.tokens is not None:
+            generation_info["token_count"] = response.meta.tokens.dict()
         return generation_info
 
     def _generate(
