6d433b32c2ea7461f9e2b23c7757897fc43bd8f8
Erick Friis <erick@langchain.dev>
2024-04-24 11:56:04 -0700

    de-beta structured output (#41)
    Co-authored-by: Harry M <127103098+harry-cohere@users.noreply.github.com>

--- libs/cohere/langchain_cohere/chat_models.py
+++ libs/cohere/langchain_cohere/chat_models.py
@@ -15,7 +15,6 @@ from typing import (
 )
 
 from cohere.types import NonStreamedChatResponse, ToolCall
-from langchain_core._api import beta
 from langchain_core.callbacks import (
     AsyncCallbackManagerForLLMRun,
     CallbackManagerForLLMRun,
@@ -244,7 +243,6 @@ class ChatCohere(BaseChatModel, BaseCohere):
         formatted_tools = _format_to_cohere_tools(tools)
         return super().bind(tools=formatted_tools, **kwargs)
 
-    @beta()
     def with_structured_output(
         self,
         schema: Union[Dict, Type[BaseModel]],
