a4b974a57a6a3d78d5047b8135205391316483eb
Erick Friis <erick@langchain.dev>
2024-04-24 11:27:31 -0700

    de-beta structured output

--- libs/cohere/langchain_cohere/chat_models.py
+++ libs/cohere/langchain_cohere/chat_models.py
@@ -192,7 +192,6 @@ class ChatCohere(BaseChatModel, BaseCohere):
         formatted_tools = _format_to_cohere_tools(tools)
         return super().bind(tools=formatted_tools, **kwargs)
 
-    @beta()
     def with_structured_output(
         self,
         schema: Union[Dict, Type[BaseModel]],
