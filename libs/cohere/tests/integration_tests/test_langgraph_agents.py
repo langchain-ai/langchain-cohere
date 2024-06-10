2a0084adc616a93caa16b2105e254d60784c5174
Anirudh31415926535 <anirudh@cohere.com>
2024-06-10 21:09:09 +0800

    chore: update replay tests (#2)
    * feat: support multi hop tool calling
    
    * refactor: deprecate create_cohere_tools_agent
    
    * test: add unit tests for multi hop tool calling
    
    * version: bump version to 0.1.7rc0
    
    * fix: lint
    
    * fix: lint fix
    
    * fix: lint
    
    * fix: lint fix
    
    * fix: minor error message fix
    
    Co-authored-by: harry-cohere <127103098+harry-cohere@users.noreply.github.com>
    
    * nit: cohere to Cohere
    
    Co-authored-by: harry-cohere <127103098+harry-cohere@users.noreply.github.com>
    
    * nit: change deprecation alternative link source and desc
    
    Co-authored-by: harry-cohere <127103098+harry-cohere@users.noreply.github.com>
    
    * chore: Ignore E501 lint fixes
    
    * chore: merge conflicrts
    
    * nit: reformat line
    
    Co-authored-by: harry-cohere <127103098+harry-cohere@users.noreply.github.com>
    
    * fix: lint and tests
    
    * nit: Add comments
    
    * fix: fix lints
    
    * fix: remove unecessary if-else check
    
    Co-authored-by: harry-cohere <127103098+harry-cohere@users.noreply.github.com>
    
    * fix: add comments and minor code cleaning
    
    * fix: small nit pick bug fixed
    
    * fix: Resolve bug where tool_results was calculated for all messages instead of just current conversation turn
    
    * fix: fix unit tests
    
    * nit: minor code cleanup and lint fixes
    
    * fix: lint fixes
    
    * chore: complete implementation of chat_history private attribute and fix tests
    
    * fix: minor code cleaning
    
    * fix: Remove chat history parameter and calculate chat history before each request
    
    * fix: linting and minor err handling
    
    * fix: nit on failing tests
    
    * nit: add comment
    
    * fix: update pending to removal in deprecated function decoration
    
    Co-authored-by: harry-cohere <127103098+harry-cohere@users.noreply.github.com>
    
    * chore: Update replay tests and increase unit test coverage
    
    * fix: lint fixes
    
    * fix: update react agent tests
    
    * fix: update tests
    
    * update test script
    
    * fix: update pyproject.toml
    
    * fix: ignore missing import
    
    * nit: fix import statement
    
    * fix: remove googleapi from replay tests
    
    * lint: fix lint
    
    * fix: move langgraph imports to inside function
    
    * lint: fix poetry lock file
    
    * lint: ignore import error mypy
    
    * fix: poetry lock changes
    
    ---------
    
    Co-authored-by: harry-cohere <127103098+harry-cohere@users.noreply.github.com>

---
+++ libs/cohere/tests/integration_tests/test_langgraph_agents.py
@@ -0,0 +1,123 @@
+"""
+Test ChatCohere chat model implementation.
+
+Uses the replay testing functionality, so you don't need an API key to run these tests.
+https://python.langchain.com/docs/contributing/testing#recording-http-interactions-with-pytest-vcr
+
+When re-recording these tests you will need to set COHERE_API_KEY.
+"""
+import sys
+from typing import Sequence, Union
+
+import pytest
+from langchain.agents import AgentExecutor, create_tool_calling_agent
+from langchain.tools import tool
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.pydantic_v1 import BaseModel, Field
+from langchain_core.tools import BaseTool, Tool
+
+from langchain_cohere import ChatCohere
+
+
+@pytest.mark.skipif(sys.version_info < (3, 9), reason="requires >= python3.9")
+@pytest.mark.vcr()
+def test_langgraph_react_agent() -> None:
+    from langgraph.prebuilt import create_react_agent  # type: ignore
+
+    @tool
+    def web_search(query: str) -> Union[int, str]:
+        """Search the web to the answer to the question with a query search string.
+
+        Args:
+            query: The search query to surf the web with
+        """
+        if "obama" and "age" in query.lower():
+            return 60
+        if "president" in query:
+            return "Barack Obama is the president of the USA"
+        if "premier" in query:
+            return "Chelsea won the premier league"
+        return "The team called Fighter's Foxes won the champions league"
+
+    @tool("python_interpeter_temp")
+    def python_tool(code: str) -> str:
+        """Executes python code and returns the result.
+        The code runs in a static sandbox without interactive mode,
+        so print output or save output to a file.
+
+        Args:
+            code: Python code to execute.
+        """
+        if "math.sqrt" in code:
+            return "7.75"
+        return "The code ran successfully"
+
+    system_message = "You are a helpful assistant. Respond only in English."
+
+    tools = [web_search, python_tool]
+    model = ChatCohere()
+
+    app = create_react_agent(model, tools, messages_modifier=system_message)
+
+    query = (
+        "Find Barack Obama's age and use python tool to find the square root of his age"
+    )
+
+    messages = app.invoke({"messages": [("human", query)]})
+
+    model_output = {
+        "input": query,
+        "output": messages["messages"][-1].content,
+    }
+    assert "7.7" in model_output.get("output", "").lower()
+
+    message_history = messages["messages"]
+
+    new_query = "who won the premier league"
+
+    messages = app.invoke({"messages": message_history + [("human", new_query)]})
+    final_answer = {
+        "input": new_query,
+        "output": messages["messages"][-1].content,
+    }
+    assert "chelsea" in final_answer.get("output", "").lower()
+
+
+@pytest.mark.skipif(sys.version_info < (3, 9), reason="requires >= python3.9")
+@pytest.mark.vcr()
+def test_langchain_tool_calling_agent() -> None:
+    def magic_function(input: int) -> int:
+        """Applies a magic function to an input."""
+        return input + 2
+
+    magic_function_tool = Tool(
+        name="magic_function",
+        description="Applies a magic function to an input.",
+        func=magic_function,
+    )
+    magic_function_tool.name = "magic_function"
+
+    class MagicFunctionInput(BaseModel):
+        input: int = Field(description="Number to apply the magic function to.")
+
+    magic_function_tool.args_schema = MagicFunctionInput
+
+    tools: Sequence[BaseTool] = [magic_function_tool]
+    model = ChatCohere()
+
+    query = "what is the value of magic_function(3)?"
+
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", "You are a helpful assistant"),
+            ("human", "{input}"),
+            # Placeholders fill up a **list** of messages
+            ("placeholder", "{agent_scratchpad}"),
+        ]
+    )
+
+    agent = create_tool_calling_agent(model, tools, prompt)
+    agent_executor = AgentExecutor(agent=agent, tools=tools)
+
+    ans = agent_executor.invoke({"input": query})
+    assert "5" in ans.get("output", "").lower()
