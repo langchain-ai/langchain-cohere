71bd1fe8117db367e0890d284563f13d33feffde
Anirudh31415926535 <anirudh@cohere.com>
2024-06-10 20:51:03 +0800

    feat: Support multi hop tool calling (#1)
    * feat: support multi hop tool calling
    
    * refactor: deprecate create_cohere_tools_agent
    
    * test: add unit tests for multi hop tool calling
    
    * version: bump version to 0.1.7rc0
    
    * fix: lint
    
    * fix: lint fix
    
    * fix: lint
    
    * fix: lint fix
    
    * fix: minor error message fix
    
    Co-authored-by: harry-cohere <127103098+harry-cohere@users.noreply.github.com>
    
    * nit: cohere to Cohere
    
    Co-authored-by: harry-cohere <127103098+harry-cohere@users.noreply.github.com>
    
    * nit: change deprecation alternative link source and desc
    
    Co-authored-by: harry-cohere <127103098+harry-cohere@users.noreply.github.com>
    
    * chore: Ignore E501 lint fixes
    
    * chore: merge conflicrts
    
    * nit: reformat line
    
    Co-authored-by: harry-cohere <127103098+harry-cohere@users.noreply.github.com>
    
    * fix: lint and tests
    
    * nit: Add comments
    
    * fix: fix lints
    
    * fix: remove unecessary if-else check
    
    Co-authored-by: harry-cohere <127103098+harry-cohere@users.noreply.github.com>
    
    * fix: add comments and minor code cleaning
    
    * fix: small nit pick bug fixed
    
    * fix: Resolve bug where tool_results was calculated for all messages instead of just current conversation turn
    
    * fix: fix unit tests
    
    * nit: minor code cleanup and lint fixes
    
    * fix: lint fixes
    
    * chore: complete implementation of chat_history private attribute and fix tests
    
    * fix: minor code cleaning
    
    * fix: Remove chat history parameter and calculate chat history before each request
    
    * fix: linting and minor err handling
    
    * fix: nit on failing tests
    
    * nit: add comment
    
    * fix: update pending to removal in deprecated function decoration
    
    Co-authored-by: harry-cohere <127103098+harry-cohere@users.noreply.github.com>
    
    * fix: update poetry lock
    
    ---------
    
    Co-authored-by: harry-cohere <127103098+harry-cohere@users.noreply.github.com>

--- libs/cohere/tests/unit_tests/test_chat_models.py
+++ libs/cohere/tests/unit_tests/test_chat_models.py
@@ -1,12 +1,17 @@
 """Test chat model integration."""
+
 import typing
 from unittest.mock import patch
 
 import pytest
 from cohere.types import NonStreamedChatResponse, ToolCall
-from langchain_core.messages import AIMessage, HumanMessage, ToolMessage
+from langchain_core.messages import AIMessage, BaseMessage, HumanMessage, ToolMessage
 
-from langchain_cohere.chat_models import ChatCohere, _messages_to_cohere_tool_results
+from langchain_cohere.chat_models import (
+    ChatCohere,
+    _messages_to_cohere_tool_results_curr_chat_turn,
+    get_cohere_chat_request,
+)
 
 
 def test_initialization() -> None:
@@ -140,7 +145,7 @@ def test_messages_to_cohere_tool_results() -> None:
         tool_call_id="d86e6098-21e1-44c7-8431-40cfc6d35590",
     )
     messages = [human_message, ai_message, tool_message]
-    results = _messages_to_cohere_tool_results(messages)
+    results = _messages_to_cohere_tool_results_curr_chat_turn(messages)
     expected = [
         {
             "call": ToolCall(name="magic_function", parameters={"input": 3}),
@@ -155,5 +160,253 @@ def test_messages_to_cohere_tool_results() -> None:
         tool_call_id="d86e6098-21e1-44c7-8431-40cfc6d35590",
     )
     messages = [human_message, ai_message, another_tool_message]
-    results = _messages_to_cohere_tool_results(messages)
+    results = _messages_to_cohere_tool_results_curr_chat_turn(messages)
     assert results == expected
+
+
+@pytest.mark.parametrize(
+    "cohere_client,messages,force_single_step,expected",
+    [
+        pytest.param(
+            ChatCohere(cohere_api_key="test"),
+            [HumanMessage(content="what is magic_function(12) ?")],
+            True,
+            {
+                "message": "what is magic_function(12) ?",
+                "chat_history": [],
+                "force_single_step": True,
+                "tools": [
+                    {
+                        "name": "magic_function",
+                        "description": "Does a magical operation to a number.",
+                        "parameter_definitions": {
+                            "a": {"description": "", "type": "int", "required": True}
+                        },
+                    }
+                ],
+            },
+            id="Single Message and force_single_step is True",
+        ),
+        pytest.param(
+            ChatCohere(cohere_api_key="test"),
+            [
+                HumanMessage(content="what is magic_function(12) ?"),
+                AIMessage(
+                    content="I will use the magic_function tool to answer the question.",
+                    additional_kwargs={
+                        "documents": None,
+                        "citations": None,
+                        "search_results": None,
+                        "search_queries": None,
+                        "is_search_required": None,
+                        "generation_id": "b8e48c51-4340-4081-b505-5d51e78493ab",
+                        "tool_calls": [
+                            {
+                                "id": "976f79f68d8342139d8397d6c89688c4",
+                                "function": {
+                                    "name": "magic_function",
+                                    "arguments": '{"a": 12}',
+                                },
+                                "type": "function",
+                            }
+                        ],
+                        "token_count": {"output_tokens": 9},
+                    },
+                    response_metadata={
+                        "documents": None,
+                        "citations": None,
+                        "search_results": None,
+                        "search_queries": None,
+                        "is_search_required": None,
+                        "generation_id": "b8e48c51-4340-4081-b505-5d51e78493ab",
+                        "tool_calls": [
+                            {
+                                "id": "976f79f68d8342139d8397d6c89688c4",
+                                "function": {
+                                    "name": "magic_function",
+                                    "arguments": '{"a": 12}',
+                                },
+                                "type": "function",
+                            }
+                        ],
+                        "token_count": {"output_tokens": 9},
+                    },
+                    id="run-8039f73d-2e50-4eec-809e-e3690a6d3a9a-0",
+                    tool_calls=[
+                        {
+                            "name": "magic_function",
+                            "args": {"a": 12},
+                            "id": "e81dbae6937e47e694505f81e310e205",
+                        }
+                    ],
+                ),
+                ToolMessage(
+                    content="112", tool_call_id="e81dbae6937e47e694505f81e310e205"
+                ),
+            ],
+            True,
+            {
+                "message": "what is magic_function(12) ?",
+                "chat_history": [
+                    {"role": "User", "message": "what is magic_function(12) ?"}
+                ],
+                "tool_results": [
+                    {
+                        "call": ToolCall(name="magic_function", parameters={"a": 12}),
+                        "outputs": [{"output": "112"}],
+                    }
+                ],
+                "force_single_step": True,
+                "tools": [
+                    {
+                        "name": "magic_function",
+                        "description": "Does a magical operation to a number.",
+                        "parameter_definitions": {
+                            "a": {"description": "", "type": "int", "required": True}
+                        },
+                    }
+                ],
+            },
+            id="Multiple Messages with tool results and force_single_step is True",
+        ),
+        pytest.param(
+            ChatCohere(cohere_api_key="test"),
+            [HumanMessage(content="what is magic_function(12) ?")],
+            False,
+            {
+                "message": "what is magic_function(12) ?",
+                "chat_history": [],
+                "tools": [
+                    {
+                        "name": "magic_function",
+                        "description": "Does a magical operation to a number.",
+                        "parameter_definitions": {
+                            "a": {"description": "", "type": "int", "required": True}
+                        },
+                    }
+                ],
+                "force_single_step": False,
+            },
+            id="Single Message and force_single_step is False",
+        ),
+        pytest.param(
+            ChatCohere(cohere_api_key="test"),
+            [
+                HumanMessage(content="what is magic_function(12) ?"),
+                AIMessage(
+                    content="I will use the magic_function tool to answer the question.",
+                    additional_kwargs={
+                        "documents": None,
+                        "citations": None,
+                        "search_results": None,
+                        "search_queries": None,
+                        "is_search_required": None,
+                        "generation_id": "91588a40-684d-40f9-ae87-e27c3b4cda87",
+                        "tool_calls": [
+                            {
+                                "id": "0ba15e974d2b4bf6bf74ba8e4b268a7a",
+                                "function": {
+                                    "name": "magic_function",
+                                    "arguments": '{"a": 12}',
+                                },
+                                "type": "function",
+                            }
+                        ],
+                        "token_count": {"input_tokens": 912, "output_tokens": 22},
+                    },
+                    response_metadata={
+                        "documents": None,
+                        "citations": None,
+                        "search_results": None,
+                        "search_queries": None,
+                        "is_search_required": None,
+                        "generation_id": "91588a40-684d-40f9-ae87-e27c3b4cda87",
+                        "tool_calls": [
+                            {
+                                "id": "0ba15e974d2b4bf6bf74ba8e4b268a7a",
+                                "function": {
+                                    "name": "magic_function",
+                                    "arguments": '{"a": 12}',
+                                },
+                                "type": "function",
+                            }
+                        ],
+                        "token_count": {"input_tokens": 912, "output_tokens": 22},
+                    },
+                    id="run-148af4fb-adf0-4f0c-b209-bffcde9a5f58-0",
+                    tool_calls=[
+                        {
+                            "name": "magic_function",
+                            "args": {"a": 12},
+                            "id": "bbec5f815a0f4c609ccb36e98c4f0455",
+                        }
+                    ],
+                ),
+                ToolMessage(
+                    content="112", tool_call_id="bbec5f815a0f4c609ccb36e98c4f0455"
+                ),
+            ],
+            False,
+            {
+                "message": "",
+                "chat_history": [
+                    {"role": "User", "message": "what is magic_function(12) ?"},
+                    {
+                        "role": "Chatbot",
+                        "message": "I will use the magic_function tool to answer the question.",
+                        "tool_calls": [
+                            {
+                                "name": "magic_function",
+                                "args": {"a": 12},
+                                "id": "bbec5f815a0f4c609ccb36e98c4f0455",
+                            }
+                        ],
+                    },
+                ],
+                "tool_results": [
+                    {
+                        "call": ToolCall(name="magic_function", parameters={"a": 12}),
+                        "outputs": [{"output": "112"}],
+                    }
+                ],
+                "tools": [
+                    {
+                        "name": "magic_function",
+                        "description": "Does a magical operation to a number.",
+                        "parameter_definitions": {
+                            "a": {"description": "", "type": "int", "required": True}
+                        },
+                    }
+                ],
+                "force_single_step": False,
+            },
+            id="Multiple Messages with tool results and force_single_step is False",
+        ),
+    ],
+)
+def test_get_cohere_chat_request(
+    cohere_client: ChatCohere,
+    messages: typing.List[BaseMessage],
+    force_single_step: bool,
+    expected: typing.Dict[str, typing.Any],
+) -> None:
+    tools = [
+        {
+            "name": "magic_function",
+            "description": "Does a magical operation to a number.",
+            "parameter_definitions": {
+                "a": {"description": "", "type": "int", "required": True}
+            },
+        }
+    ]
+
+    result = get_cohere_chat_request(
+        messages,
+        stop_sequences=cohere_client.stop,
+        force_single_step=force_single_step,
+        tools=tools,
+    )
+
+    # Check that the result is a dictionary
+    assert isinstance(result, dict)
+    assert result == expected
