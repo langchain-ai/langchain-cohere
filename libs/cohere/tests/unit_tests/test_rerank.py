bd90d84aae2660831415b31df8228d75b3e56b39
Anirudh31415926535 <anirudh@cohere.com>
2024-07-30 20:31:27 +0800

    feat: make the model parameter compulsory for embedding and rerankers (#75)
    This PR introduces a breaking change, which makes the model parameters
    compulsory for embedding and rerank.
    This ensures that the model does not get bumped/altered unbeknownst to
    the user, and ensures that the user knows the model underlying these
    implementations.

--- libs/cohere/tests/unit_tests/test_rerank.py
+++ libs/cohere/tests/unit_tests/test_rerank.py
@@ -5,4 +5,4 @@ from langchain_cohere import CohereRerank
 
 def test_initialization() -> None:
     """Test chat model initialization."""
-    CohereRerank(cohere_api_key="test")
+    CohereRerank(cohere_api_key="test", model="rerank-english-light-v3.0")
