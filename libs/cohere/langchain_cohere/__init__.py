2426566679a12027238ca49586302e07c2b3432d
Anirudh31415926535 <anirudh@cohere.com>
2025-01-22 17:11:05 +0800

    Remove the create_csv_agent abstraction (#25)
    * Remove the csv agent
    
    * update poetry lock file
    
    * Update poetry toml version
    
    * Remove dependencies on pandas and tabulate
    
    * Update poetry lock
    
    * Update tests

--- libs/cohere/langchain_cohere/__init__.py
+++ libs/cohere/langchain_cohere/__init__.py
@@ -2,7 +2,6 @@ from langchain_cohere.chains.summarize.summarize_chain import load_summarize_cha
 from langchain_cohere.chat_models import ChatCohere
 from langchain_cohere.cohere_agent import create_cohere_tools_agent
 from langchain_cohere.common import CohereCitation
-from langchain_cohere.csv_agent.agent import create_csv_agent
 from langchain_cohere.embeddings import CohereEmbeddings
 from langchain_cohere.rag_retrievers import CohereRagRetriever
 from langchain_cohere.react_multi_hop.agent import create_cohere_react_agent
@@ -17,7 +16,6 @@ __all__ = [
     "CohereRerank",
     "create_cohere_tools_agent",
     "create_cohere_react_agent",
-    "create_csv_agent",
     "create_sql_agent",
     "load_summarize_chain",
 ]
