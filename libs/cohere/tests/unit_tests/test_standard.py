d3490d66b951c4419d7e061db4d0da7ce234c367
Henning-Kubatzsch <henningkb@outlook.com>
2025-07-23 17:10:43 +0200

    fixed li errors

--- libs/cohere/tests/unit_tests/test_standard.py
+++ libs/cohere/tests/unit_tests/test_standard.py
@@ -1,12 +1,8 @@
 """Standard LangChain interface tests"""
 
-from typing import Any, Type
-
-import pytest
-from langchain_core.language_models import BaseChatModel
-#from langchain_standard_tests.unit_tests import ChatModelUnitTests
 from pydantic import BaseModel, Field
 
+from langchain_core.language_models import BaseChatModel
 from langchain_cohere import ChatCohere
 
 
@@ -16,35 +12,6 @@ class Person(BaseModel):
     name: str = Field(..., description="The name of the person.")
     age: int = Field(..., description="The age of the person.")
 
-
-# class TestCohereStandard:
-#    @property
-#    def chat_model_class(self) -> Type[BaseChatModel]:
-#        return ChatCohere
-
-#    @property
-#    def chat_model_params(self) -> dict:
-#        return {
-#            "model": "command-r-plus",
-#            "temperature": 0,
-#            "cohere_api_key": "test_key",
-#        }
-
-#    @pytest.mark.xfail(reason="Standard test not moved to pydantic V2...")
-#    @pytest.mark.parametrize("schema", [Person, Person.model_json_schema()])
-#    def test_with_structured_output(
-#        self,
-#        model: BaseChatModel,
-#        schema: Any,
-#    ) -> None:
-#        if not self.has_structured_output:
-#            return
-#
-#        assert model.with_structured_output(schema) is not None
- 
-
-from langchain_core.language_models import BaseChatModel
-from langchain_cohere import ChatCohere
 
 def test_chat_model_can_instantiate():
     model = ChatCohere(
